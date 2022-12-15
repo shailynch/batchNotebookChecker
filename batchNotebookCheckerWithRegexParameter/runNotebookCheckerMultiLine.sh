3464208fd63dc1b606db209175f6981f3f10bf55
Shai Lynch <shai.lynch@gmail.com>
2022-12-15 08:29:15 +0000

    update folders

--- batchNotebookCheckerWithRegexParameter/runNotebookCheckerMultiLine.sh
+++ batchNotebookCheckerWithRegexParameter/runNotebookCheckerMultiLine.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # run this to see how the program operates, the below command is the command that a terminal shelll would require
-papermill notebookChecker.ipynb outputNotebook.ipynb -p notebookList '''./sampleNotebooks/sample1.ipynb
+papermill notebookChecker.ipynb outputNotebookMultiLine.ipynb -p notebookList '''./sampleNotebooks/sample1.ipynb
 ./sampleNotebooks/sample2.ipynb
 ./sampleNotebooks/sample3.ipynb''' -p disallowedTermsList '''
 0,security,100,security des
@@ -12,4 +12,3 @@ papermill notebookChecker.ipynb outputNotebook.ipynb -p notebookList '''./sample
 1,(?P<url>https?://[^\s]+),50,URL des
 2,\d+,50,Hard-coded Number des
 '''
-
